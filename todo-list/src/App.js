35bf82bf816da3cfc4f50221e5f773022f381dca
GOHULRAJ <gohulrajs71@gmail.com>
2023-06-10 14:57:42 +0530

    todo-list

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,17 +1,39 @@
-import './App.css';
+//import './App.css';
+import React,{Component} from 'react';
+export default class FormTodo extends Component{
+  state={
+    inputValue:"",
+    todos:[]
+  };
+  inputChange=(e)=>{
+    this.setState({inputValue:e.target.value});
+  };
+  buttonSubmit=(e)=>{
+    e.preventDefault();
+    if(this.state.inputValue!==""){
+      this.setState({
+        todos:[this.state.inputValue,...this.state.todos],
+        inputValue:""
 
-function App() {
-  return (
-    <div className="App">
-    1.Header
+      });
+    }
+  };
+  render(){
+    return(
+      <form className='form' onSubmit={this.buttonSubmit}>
+        <p className='text' style={{textAlign:"left"}}>Enter the known Languages</p>
+        <input className='input' type='text' value={this.state.inputValue} placeholder="Enter task..." onChange={this.inputChange}></input><br></br>
+        <button onClick={this.buttonSubmit} className='button' >SUBMIT</button>
+        <ol className='list'>{this.state.todos.map((todo,index)=>(
+          <li key={index} onClick={this.listRemove}>
+            {todo}
+          </li>
+          
 
-
-    2. Inputs
-
-
- 
-    </div>
-  );
+        ))}
+          </ol>
+        </form>
+    )
+  }
 }
 
-export default App;
